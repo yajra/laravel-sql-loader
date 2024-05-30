0756c9c241f5e7006946d966c20acfa322e8e4de
Arjay Angeles <aqangeles@gmail.com>
2024-05-30 11:26:25 +0800

    feat: publish config

--- src/SQLLoaderServiceProvider.php
+++ src/SQLLoaderServiceProvider.php
@@ -9,5 +9,11 @@ class SQLLoaderServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->mergeConfigFrom(__DIR__.'/config/sql-loader.php', 'sql-loader');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/sql-loader.php' => config_path('sql-loader.php'),
+            ], 'config');
+        }
     }
 }
